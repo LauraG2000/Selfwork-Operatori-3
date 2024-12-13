edbb20c28600f10dd5e22ef948a13a253522b900
LauraG2000 <lauragag19@gmail.com>
2024-12-13 19:49:33 +0100

    eseguito compito boperatori 3

---
+++ OPERATORI3/Selfwork-operatori3.js
@@ -0,0 +1,16 @@
+let num1 = 10;
+let num2 = 50;
+
+// Calcoli
+let somma = num1 + num2;
+let sottrazione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione = num1 / num2;
+let potenza = num1 ** num2;
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono:`);
+console.log(`Somma: ${somma}`);
+console.log(`Sottrazione: ${sottrazione}`);
+console.log(`Moltiplicazione: ${moltiplicazione}`);
+console.log(`Divisione: ${divisione}`);
+console.log(`Potenza: ${potenza}`);
